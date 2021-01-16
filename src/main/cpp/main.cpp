7c57e11c00aed277eadab50027a8a4234bd6b4ce
exqudens <exqudens@gmail.com>
2021-01-16 00:18:46 -0800

    step 9

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,11 +1,15 @@
 #include "exqudens/util/StringUtils.hpp"
 
 #include <string>
+#include <cstdlib>
+#include <filesystem>
 #include <iostream>
 
-int main(int argc, char** argv) {
-  using String      = std::string;
-  using StringUtils = exqudens::util::StringUtils;
+using String      = std::string;
+using Path        = std::filesystem::path;
+using StringUtils = exqudens::util::StringUtils;
+
+void trim() {
 
   String string = "\n aaa bbb\n   ";
   String expected = "aaa bbb";
@@ -23,8 +27,29 @@ int main(int argc, char** argv) {
     message += actual;
     message += "'";
     std::cerr << message << std::endl;
-    return 1;
-  } else {
-    return 0;
   }
+}
+
+void parentPath(String string) {
+  if(const char* env_p = std::getenv("PATH")) {
+    std::cout << "Your PATH is: '" << env_p << "'" << std::endl;
+  }
+  String s = Path(string.c_str()).parent_path().string();
+  std::cout << "Parent PATH is: '" << s << "'" << std::endl;
+}
+
+int main(int argc, char** argv) {
+  if (argc > 1) {
+
+    String command = String(argv[1]);
+
+    if ("--trim" == command) {
+      trim();
+    } else if ("--parent-path" == command) {
+      parentPath(String(argv[2]));
+    }
+
+  }
+
+  return 0;
 }
